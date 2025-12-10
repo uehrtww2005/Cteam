78e65c1b54879b11ab388bf6955bfaf8418eafb3
bubuka3150 <knz2474539@stu.o-hara.ac.jp>
2025-12-10 10:24:27 +0900

    クーポンできたよ

---
+++ src/bean/Coupon.java
@@ -0,0 +1,37 @@
+package bean;
+
+public class Coupon {
+
+	private int couponId;
+	private int storeId;  //storeIdは外部キー
+	private String couponIntroduct;
+	private String couponRank;
+
+
+	public int getCouponId() {
+		return couponId;
+	}
+	public void setCouponId(int couponId) {
+		this.couponId = couponId;
+	}
+	public int getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(int storeId) {
+		this.storeId = storeId;
+	}
+	public String getCouponIntroduct() {
+		return couponIntroduct;
+	}
+	public void setCouponIntroduct(String couponIntroduct) {
+		this.couponIntroduct = couponIntroduct;
+	}
+	public String getCouponRank() {
+		return couponRank;
+	}
+	public void setCouponRank(String couponRank) {
+		this.couponRank = couponRank;
+	}
+
+
+}
