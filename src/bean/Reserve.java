a7989b63791c3d42ebb18361a473eed0365ccc73
bubuka3150 <knz2474539@stu.o-hara.ac.jp>
2026-01-14 10:30:47 +0900

    永遠へ、新年あけましておめでとうございます。元気に過ごしておりますでしょうか。こっちは元気ですよ。お金ください

---
+++ src/bean/Reserve.java
@@ -0,0 +1,110 @@
+package bean;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class Reserve {
+
+	private Integer reservationId;
+    private Integer storeId;
+    private Integer userId;
+    private Integer groupId;
+    private LocalDateTime reservedAt;  // ← DATETIME
+    private String customerName;
+    private String customerTel;
+    private Integer numPeople;
+    private Integer advancePay;
+    private Integer totalPay;
+    private Timestamp createdAt;   // ★追加
+
+
+
+	public Integer getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(Integer reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public Integer getStoreId() {
+		return storeId;
+	}
+
+	public void setStoreId(Integer storeId) {
+		this.storeId = storeId;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(Integer groupId) {
+		this.groupId = groupId;
+	}
+
+	public LocalDateTime getReservedAt() {
+		return reservedAt;
+	}
+
+	public void setReservedAt(LocalDateTime reservedAt) {
+		this.reservedAt = reservedAt;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerTel() {
+		return customerTel;
+	}
+
+	public void setCustomerTel(String customerTel) {
+		this.customerTel = customerTel;
+	}
+
+	public Integer getNumPeople() {
+		return numPeople;
+	}
+
+	public void setNumPeople(Integer numPeople) {
+		this.numPeople = numPeople;
+	}
+
+	public Integer getAdvancePay() {
+		return advancePay;
+	}
+
+	public void setAdvancePay(Integer advancePay) {
+		this.advancePay = advancePay;
+	}
+
+	public Integer getTotalPay() {
+		return totalPay;
+	}
+
+	public void setTotalPay(Integer totalPay) {
+		this.totalPay = totalPay;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+
+}
